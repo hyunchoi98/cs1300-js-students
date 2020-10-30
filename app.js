17cec2c75ee91273260ce8232ab436e14e6661b3
hyunchoi98 <hyunchoi98@gmail.com>
2020-10-29 20:21:22 -0400

    blah

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=09Rc-MndEISM8wpUoPlQfIDVFgUVUJyC_X-jjrsrOP4";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,60 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      const plantData = JSON.parse(request.response).data;
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const search = () =>
+  new Promise((resolve, reject) => {
+    const request = doCORSRequest({
+      url: "https://trefle.io/api/v1/plants/search" + apiToken + "&q=" + document.getElementById('name').value,
+    });
+    resolve(request);
+  });
+
+const search2 = () => {
+  search().then(
+    (request) => 
+    (request.onload = request.onerror = () => {
+      const plantData = JSON.parse(request.response).data;
+      const plantHTML = plantData.map(plant => plantDiv(plant));
+
+      console.log(plantHTML);
+
+      let results = document.getElementById('results');
+      results.innerHTML = '';
+      console.log(results.length);
+      for (let i = 0; i < plantData.length; i++) {
+        results.append(plantHTML[i]);
+      }
+      console.log(plantData);
+    })
+  )
+
+};
+
+const plantDiv = (plantJS) => {
+  const newDiv = document.createElement("div"); 
+  
+  const name = document.createTextNode('Name: ' + plantJS['common_name']);
+  const family = document.createTextNode('Family: ' + plantJS['family'] );
+  const genus = document.createTextNode('Genus: ' + plantJS['genus']);
+  var img = document.createElement('img');
+  img.width = '200';
+  img.src = plantJS['image_url'];
+  
+  // add the text node to the newly created div
+  newDiv.appendChild(img);
+  newDiv.appendChild(document.createElement('br'));
+  newDiv.appendChild(name);
+  newDiv.appendChild(document.createElement('br'));
+  newDiv.appendChild(family);
+  newDiv.appendChild(document.createElement('br'));
+  newDiv.appendChild(genus);
+  newDiv.appendChild(document.createElement('br'));
+
+  newDiv.style.margin = "50px 10px"
+  return newDiv;
+}
